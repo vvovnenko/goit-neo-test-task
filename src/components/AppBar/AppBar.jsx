c56103e77ca55b3099cc30647f2cfafc8beeade7
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-09-14 18:50:45 +0300

    add style for app bar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,24 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import css from "./AppBar.module.css";
+import Logo from "../Logo/Logo.jsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 const AppBar = () => {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/catalog">Catalog</NavLink>
+    <div className={css.bar}>
+      <span className={css.logo}>
+        <Logo />
+      </span>
+      <NavLink className={buildLinkClass} to="/" end>
+        Home
+      </NavLink>
+      <NavLink className={buildLinkClass} to="/catalog" end>
+        Catalog
+      </NavLink>
     </div>
   );
 };
